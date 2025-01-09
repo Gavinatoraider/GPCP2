3126219b0c186d79627cfce0a823764e3700e6b6
Gavinatoraider <gavin.pierce@ucas-edu.net>
2025-01-09 13:44:08 -0700

    \
    \
    \

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,5 +1,15 @@
 # Gavin Pierce Financial Calculator
-y=1
+#this is the main file and will hvae the major amounts of code.
 def main():
-    y=y+1
-print ("hi")
+    Acount_Amount=int(input("what is your back acounts amount right now? "))
+    #this will caslculate your intrest.
+    def intrest_calculator():
+        
+        Amount_with_intrest=Acount_Amount*.06
+        print(Amount_with_intrest, "is you amount of intrest")
+        intrest_Amount=Amount_with_intrest+Acount_Amount
+        print(f"{intrest_Amount} this is your amount with incest.")
+
+    intrest_calculator()
+    pass
+main()
