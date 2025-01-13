ba10439bea92f53bf8cd5a8da7a6a1c1a38a5473
Gavinatoraider <gavin.pierce@ucas-edu.net>
2025-01-13 13:15:43 -0700

    addes intrest calculator

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,15 +1,19 @@
 # Gavin Pierce Financial Calculator
 #this is the main file and will hvae the major amounts of code.
 def main():
-    Acount_Amount=int(input("what is your back acounts amount right now? "))
+    Acount_Amount=int(input("what is your bank acounts amount right now? "))
     #this will caslculate your intrest.
-    def intrest_calculator():
-        
-        Amount_with_intrest=Acount_Amount*.06
-        print(Amount_with_intrest, "is you amount of intrest")
-        intrest_Amount=Amount_with_intrest+Acount_Amount
-        print(f"{intrest_Amount} this is your amount with incest.")
-
-    intrest_calculator()
+    monthly_income= 
+def intrest_calculator():
+    global Acount_Amount
+    Amount_with_intrest=Acount_Amount*.06
+    print(Amount_with_intrest, "is you amount of intrest")
+    intrest_Amount=Amount_with_intrest+Acount_Amount
+    print(f"{intrest_Amount} this is your amount with intrest.")
     pass
+
+def budget_allocator():
+    monthly_income= int(input("what is you income per mounth? "))
+    pass   
+    
 main()
---
+++ list type notes/main.py
@@ -0,0 +1,4 @@
+fam = ("Gage", "Gavin", "Mother", "Father")
+
+print(fam.index("Gage"))
+
