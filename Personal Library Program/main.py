3d377a20b44fa4c15742a3a718057e114171f2ab
Gavinatoraider <gavin.pierce@ucas-edu.net>
2025-01-16 13:43:54 -0700

    added mucry list

--- Personal Library Program/main.py
+++ Personal Library Program/main.py
@@ -1,9 +1,10 @@
 # Gavin Pierce Personal Library Program
 
 music = ("Wake Up" , "Take Me to the Beach" , "Don't Forget Me" , "Nace to Meet You" , "In Your Corner" , "Kid" , "Eyes Closed" , "Gods Don't Pray" , "Fire in These Hills" , "Enemy" , "Wrecked" , "Easy Come Easy Go" , "Dull Knives" , "No Time For Toxic People" , "Symphony" , "Blur" < "Take it Easy" , "Ferris Wheel" , "Tied" , "Continaul" , "My Life" , "Monday" , "Giants" , "Follow You" , "One Day")
+songs = ["My Life", "Bones", "Wait for Me", "Wave", "Cutthroat", "Scatter", "I'm Happy", "Lonely", "Follow You", "Giants", "Enemy", "Wrecked", "Bones (Reimagined)", "Sharks", "Dull Knives (with JID)", "Crushed", "Take the World", "Daydreamer", "Change"]
 
 def main():
 
-    f
+    print(music)
 
 main()
