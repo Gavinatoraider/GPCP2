7e6a12677f60ab80890a0ede3786bd0d4cfb09d3
Gavinatoraider <gavin.pierce@ucas-edu.net>
2025-01-16 13:35:41 -0700

    Personal Library Program

---
+++ Personal Library Program/main.py
@@ -0,0 +1,9 @@
+# Gavin Pierce Personal Library Program
+
+music = ("Wake Up" , "Take Me to the Beach" , "Don't Forget Me" , "Nace to Meet You" , "In Your Corner" , "Kid" , "Eyes Closed" , "Gods Don't Pray" , "Fire in These Hills" , "Enemy" , "Wrecked" , "Easy Come Easy Go" , "Dull Knives" , "No Time For Toxic People" , "Symphony" , "Blur" < "Take it Easy" , "Ferris Wheel" , "Tied" , "Continaul" , "My Life" , "Monday" , "Giants" , "Follow You" , "One Day")
+
+def main():
+
+    f
+
+main()
