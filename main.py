dcbadedac55bca3ebe19d9ac7b9e8dddf85c8395
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2025-01-23 13:02:20 -0700

    Added ticket main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Jackson Hauley, Gavin Pierce, Lizzy Saldana, Luke Murdock
 
 import random
+import time
 
 # Initializing Variables
 artist_list = []
 tickets = []
+id = 0
 
 
 # Defining Funcitons
@@ -12,11 +14,11 @@ tickets = []
 def main():
     while True:
         cs()
-        choice = int_input("MUSIC FESTIVAL\n\n1. Information\n2. Tickets\n3. Schedule\n4. Artists\n5. Recommendation\n6. Exit")
+        choice = int_input("MUSIC FESTIVAL\n\n1. Information\n2. Tickets\n3. Schedule\n4. Artists\n5. Recommendation\n6. Exit\n\nChoose one (1-6): ")
         if choice == 1: # Information
             pass
         elif choice == 2: # Tickets
-            pass
+            ticket_main()
         elif choice == 3: # Schedule
             pass
         elif choice == 4: # Artist List
@@ -55,6 +57,21 @@ def float_input(text): # Only takes in floats
             continue
         return output
 
+def ticket_main(): # Ticket main function (runs all ticket information through here) ((jacksons function))
+    while True:
+        cs()
+        choice = int_input("TICKET MENU\n\n1. Buy Ticket\n2. Search Tickets\n3. Ticket Informatoin\n4. Ticket Sales Report\n5. Exit\n\nChoose one (1-5): ")
+        if choice == 1: # Buy Ticket
+            buy_ticket()
+        elif choice == 2: # Search Tickets
+            pass
+        elif choice == 3: # Ticket Informatoin
+            pass
+        elif choice == 4: # Ticket Sales Report
+            pass
+        elif choice == 5: # Exit
+            main()
+
 def buy_ticket(): # Buy a ticket (Jacksons Function)
     ticket = []
     cs()
@@ -83,26 +100,62 @@ def buy_ticket(): # Buy a ticket (Jacksons Function)
         else:
             print("Invalid Input (1-3)")
             input("Press enter to continue")
-    print(f"Membership {membership} selected") # Visual
+    while True:
+        duration_choice = int_input("\nHow long do you want this ticket to work?\n1. 1 day\n2. 3 days\n3. 1 week\n4. 1 month\n5. season pass\n\nType number here (1-5): ") # Duration Selection
+        if duration_choice == 1: # These are for how long your ticket works
+            duration = "1 day" # 1 day duration
+            cost += 0
+            break
+        elif duration_choice == 2:
+            duration = "3 days" # 3 days duration
+            cost += 40
+            break
+        elif duration_choice == 3:
+            duration = "1 week" # 1 week duration
+            cost += 80
+            break
+        elif duration_choice == 4:
+            duration = "1 month"  # 1 month duration
+            cost += 120
+            break
+        elif duration_choice == 5:
+            duration = "season pass" # season pass
+            cost += 300
+            break
+        else:
+            print("Invalid Input (1-3)") # Error Handling
+            input("Press enter to continue")
+
+    print(f"\nMembership {membership} selected") # Visual
+    print(f"This ticket will last {duration}!")
     print(f"Cost: ${cost}")
-    input("\nType yes to confirm, anything else will cancel: ") # Confirming Information
-    if input == "yes":
+    quick_choice1 = str_input("\nType yes to confirm, anything else will cancel: ") # Confirming Information
+    if quick_choice1 == "yes":
         cs()
         print(f"Buying {membership} ticket") # Buying Ticket
-        creditcard = int_input("Enter credit card number, with no spaces (1234123412341234): ")
-        ccv = int_input("Enter CVV number, with no spaces (123): ")
-        input("\nType yes to confirm, anything else will cancel: ") # Confirming Information
-        if input == "yes":
+        creditcard = int_input("Enter credit card number, with no spaces (1234123412341234): ") # Getting money
+        cvv = int_input("Enter CVV number, with no spaces (123): ")
+        quick_choice2 = str_input("\nType yes to confirm, anything else will cancel: ") # Confirming Information
+        if quick_choice2 == "yes":
             print("\nTicket Bought!\n")
+            id += 1
+            print(f"Ticket ID: {id}")
             print(f"""
--------------------------------------------------------------------
-|  ///-\\\\ | Name: {firstname} {lastname}
-|  |^   ^|  | Age: {age}
-|  |O   O|  |
-|  |  ~  |  |
-|   \ U /   |
-|    | |    |
--------------------------------------------------------------------""")
+████████████████████████████████████████████████
+█   WMWMW   | Name: {firstname:<15} {lastname:<15} █
+█  /     \\  | Age: {age:<5}                      █
+█  |O   O|  | Membership Level: {membership:<5}   █
+█  |  L  |  | Date Bought: {time.ctime():<20}      █
+█   \ W /   | Duration: {duration:<15}             █
+█    | |    | ID: {id:<10}                         █
+████████████████████████████████████████████████""") # Printing out their ticket
+            input("Press enter to continue")
+        else:
+            print("Ticket Canceled!")
+            input("Press enter to continue")
+    else:
+        print("Ticket Canceled!")
+        input("Press enter to continue")
     
 
 
