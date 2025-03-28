a0d48b3fa758204895eaabe09274afffaf31549d
Gavinatoraider <gavin.pierce@ucas-edu.net>
2025-03-28 13:39:04 -0600

    I added diffrent Csvs for each of the currencys

--- coin_change_problem/currency_changer.py
+++ coin_change_problem/currency_changer.py
@@ -8,13 +8,14 @@ British pound
 this is housed in the money csv
 """
 
+currency_type=["USD","Euro","Yen","British Pound"]
 
 print("welcome to the currency changer")
 
-first_coin_type=input("what is the currency you trying to exchange from? (if you have the same to it will try to exchange to the smallest form of change) ")
+coin_type=input("what is the currency you trying to exchange from? (USD, Euro, Yen and British pound. these are the only valid options) (if you have the same to it will try to exchange to the smallest form of change) ")
 
-second_coin_type=input("what is the currency you are trying to exgange to? (if you have the same to it will try to exchange to the smallest form of change) ")
+if coin_type not in currency_type:
+    print("you need a valid currency.")
 
-first_coin_change=input("what is the amount you of money you want to use in ")
+coin_change_amount=input("what is the amount you of money you want to use in ", coin_type, "?")
 
-second_coin_change=input
